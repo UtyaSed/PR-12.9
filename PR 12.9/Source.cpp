844b894ae4205793c5a09623c38f0d7552f41c89
Viktor <vbonchykv@gmail.com>
2021-05-30 13:50:00 +0300

    first master commit

--- PR 12.9/Source.cpp
+++ PR 12.9/Source.cpp
@@ -7,27 +7,57 @@ struct Node
 		* right;
 	Info info;
 };
+
 Node* CreateTree(int nodeCount);
+int Count(Node* root);
+Node* BinarySearchInsert(Node*& root, Info value, bool& found);
 void PrintTree(Node* root, int level);
-void InfixOrder(Node* root, Info x, bool& answ);
+void RightRotation(Node*& root);
+void LeftRotation(Node*& root);
+int Height(Node* root);
+int BFactor(Node* root);
+void BalanceHeight(Node*& root);
+Node* BinarySearchDelete(Node* root, Info value);
 
 int main()
 {
 	int N;
 	cout << "Enter nodes count: "; cin >> N;
-	Node* root = CreateTree(N);
-	PrintTree(root, 1);
-	int k;
-	cout << "Input k: "; cin >> k;
-	bool answ = false;
-	InfixOrder(root, k, answ);
-	if (answ == true) {
-		cout << "Yes" << endl;
+
+
+	Node* root = NULL;
+	Info value;
+	bool found = false;
+	for (int i = 0; i < N; i++)
+	{
+		cout << "value: "; cin >> value;
+		BinarySearchInsert(root, value, found); 
 	}
-	else
-		cout << "No" << endl;
+
+	cout << endl;
+	PrintTree(root, 0);
+	cout << endl;
+
+	cout << "Balanced tree:" << endl << endl;
+	BalanceHeight(root);
+
+	cout << endl;
+	PrintTree(root, 0);
+	cout << endl;
+
+	int dn;
+	cout << "enter node value you want to delete:"; cin >> value;
+	BinarySearchDelete(root, value);
+
+	cout << endl;
+	PrintTree(root, 0);
+	cout << endl;
+
+
+	cout << "nodes count: " << Count(root) << endl;
 	return 0;
 }
+
 Node* CreateTree(int nodeCount)
 {
 	if (nodeCount == 0)
@@ -43,7 +73,139 @@ Node* CreateTree(int nodeCount)
 		newNode->right = CreateTree(rightCount);
 		return newNode;
 	}
+
 }
+
+int Count(Node* root)
+{
+	if (root == NULL)
+		return 0;
+	else
+		return 1 + Count(root->left) + Count(root->right);
+}
+
+Node* BinarySearchInsert(Node*& root, Info value, bool& found)
+{
+	if (root == NULL)
+	{
+		root = new Node;
+		root->info = value;
+		root->left = NULL;
+		root->right = NULL;
+		found = false;
+		return root;
+	}
+	else
+		if (value == root->info)
+		{
+			found = true;
+			return root;
+		}
+		else
+			if (value < root->info)
+				return BinarySearchInsert(root->left, value, found);
+			else
+				return BinarySearchInsert(root->right, value, found);
+}
+
+void RightRotation(Node*& root)
+{
+	Node* tmp1 = root->left;
+	Node* tmp2 = tmp1->right;
+	tmp1->right = root; 
+	root->left = tmp2; 
+	root = tmp1; 
+}
+void LeftRotation(Node*& root)
+{
+	Node* tmp1 = root->right;
+	Node* tmp2 = tmp1->left;
+	tmp1->left = root; 
+	root->right = tmp2; 
+	root = tmp1; 
+}
+int Height(Node* root)
+{
+	if (root == NULL)
+		return 0;
+	int hL = Height(root->left);
+	int hR = Height(root->right);
+	return (hL > hR ? hL : hR) + 1;
+}
+int BFactor(Node* root)
+{
+	return Height(root->right) - Height(root->left);
+}
+void BalanceHeight(Node*& root)
+{
+	if (root != NULL)
+	{
+		while (BFactor(root) >= 2)
+		{
+			if (BFactor(root->right) < 0)
+				RightRotation(root->right);
+			LeftRotation(root);
+		}
+		while (BFactor(root) <= -2)
+		{
+			if (BFactor(root->left) > 0)
+				LeftRotation(root->left);
+			RightRotation(root);
+		}
+		BalanceHeight(root->left);
+		BalanceHeight(root->right);
+	}
+}
+
+Node* FindMax(Node* root)
+{
+	if (root->right != NULL)
+		return FindMax(root->right);
+	else
+		return root;
+}
+
+Node* BinarySearchDelete(Node* root, Info value)
+{
+	if (NULL == root) return NULL; 
+	if (root->info == value)
+	{
+		if (NULL == root->left && NULL == root->right) 
+		{
+			delete root;
+			return NULL;
+		}
+		if (NULL == root->right && root->left != NULL) 
+		{ 
+			Node* temp = root->left;
+			delete root;
+			return temp;
+		}
+		if (NULL == root->left && root->right != NULL)
+		{ 
+			Node* temp = root->right;
+			delete root;
+			return temp;
+		}
+	
+		root->info = FindMax(root->left)->info;
+		root->left = BinarySearchDelete(root->left, root->info);
+		return root;
+	}
+	
+	if (value < root->info)
+	{ 
+		root->left = BinarySearchDelete(root->left, value);
+		return root;
+	}
+	if (value > root->info)
+	{
+		root->right = BinarySearchDelete(root->right, value);
+		return root;
+	}
+	return root;
+}
+
 void PrintTree(Node* root, int level)
 {
 	if (root != NULL)
@@ -53,18 +215,5 @@ void PrintTree(Node* root, int level)
 			cout << " ";
 		cout << root->info << endl;
 		PrintTree(root->left, level + 1);
-	}
-}
-
-void InfixOrder(Node* root, Info x, bool& answ)
-{
-	if (root != NULL)
-	{
-		if (root->info == x) {
-			answ = true;
-			return;
-		}
-		InfixOrder(root->left, x, answ);
-		InfixOrder(root->right, x, answ);
 	}
 }
